16adc8171a67bf210b2ba1c1a210e02c44fdbcb7
Brijesh <softbrijeshio@gmail.com>
2026-02-17 11:12:04 +0530

    feat: Set up Prisma ORM with initial database schema and NestJS integration.

--- api/src/config/env.config.ts
+++ api/src/config/env.config.ts
@@ -3,4 +3,5 @@ import 'dotenv/config';
 export const envConfig = {
   PORT: process.env.PORT || 3000,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  DATABASE_URL: process.env.DATABASE_URL,
 };
