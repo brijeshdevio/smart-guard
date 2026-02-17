941169be4f14ff44e83ad3f223a6ffc7a190c4f4
Brijesh <softbrijeshio@gmail.com>
2026-02-17 11:19:58 +0530

    feat: Implement API security and cross-origin resource sharing with cookie parsing, Helmet, and CORS.

--- api/src/config/env.config.ts
+++ api/src/config/env.config.ts
@@ -4,4 +4,5 @@ export const envConfig = {
   PORT: process.env.PORT || 3000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL,
+  FRONTEND_URL: process.env.FRONTEND_URL,
 };
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { envConfig } from './config';
 
@@ -6,6 +8,12 @@ const PORT = envConfig.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.enableCors({
+    origin: envConfig.FRONTEND_URL,
+    credentials: true,
+  });
+  app.use(helmet());
   await app.listen(PORT);
 }
 
